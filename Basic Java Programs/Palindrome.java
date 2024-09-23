08ecc9e22ea65df5eeeb62f6fce0333c3d29422c
MitaliSPandhe <122465899+MitaliSPandhe@users.noreply.github.com>
2024-09-23 12:13:20 +0530

    Palindrome.java

--- Basic Java Programs/Palindrome.java
+++ Basic Java Programs/Palindrome.java
@@ -51,25 +51,25 @@ class Palindrome
 //     }
 // }
 
-import java.util.Scanner;
- class Palindrome{
-    public static void main(String args[]){
-        int n ,temp,s=0;
-        System.out.println("Enter number: ");
-        Scanner sc=new Scanner(System.in);
-        n=sc.nextInt();
-        temp=n;
+// import java.util.Scanner;
+//  class Palindrome{
+//     public static void main(String args[]){
+//         int n ,temp,s=0;
+//         System.out.println("Enter number: ");
+//         Scanner sc=new Scanner(System.in);
+//         n=sc.nextInt();
+//         temp=n;
 
-        while(n>0){
-            int remainder=n%10;  // 121%10==1   12%10==2    1%10==1
-            s=(s*10)+remainder;  // 0*10+1==1   1*10+2==12  12*10+1==121
-            n=n/10;              // 121/10==12  12/10==1    1/10==0
-        }
-        if(temp==s){
-            System.out.print("palindrome");
-        }
-        else{
-            System.out.print("not palindrome");
-        }
-    }
- }
+//         while(n>0){
+//             int remainder=n%10;  // 121%10==1   12%10==2    1%10==1
+//             s=(s*10)+remainder;  // 0*10+1==1   1*10+2==12  12*10+1==121
+//             n=n/10;              // 121/10==12  12/10==1    1/10==0
+//         }
+//         if(temp==s){
+//             System.out.print("palindrome");
+//         }
+//         else{
+//             System.out.print("not palindrome");
+//         }
+//     }
+//  }
